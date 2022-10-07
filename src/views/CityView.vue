9ecdf3f5e91aa5dfbdb23795013c040caa45fb36
triggerX <moudourousamuel@gmail.com>
2022-10-07 18:08:24 +0100

    complete app

---
+++ src/views/CityView.vue
@@ -0,0 +1,16 @@
+<template >
+    <div>
+        <Suspense>
+            <AsyncCityView/>
+            <template #fallback>
+                <CityViewSkeletonVue />
+            </template>
+        </Suspense>
+    </div>
+</template>
+
+<script setup>
+import AsyncCityView from '../components/AsyncCityView.vue';
+import CityViewSkeletonVue from '../components/CityViewSkeleton.vue';
+
+</script>
